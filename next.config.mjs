6316ff868483c2be7c8780c7e87bcb3da5b075cd
puribehuig-boop <puribehuig@gmail.com>
2025-10-07 14:42:37 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,3 @@
-import type { NextConfig } from 'next';
-const nextConfig: NextConfig = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = { reactStrictMode: true };
 export default nextConfig;
